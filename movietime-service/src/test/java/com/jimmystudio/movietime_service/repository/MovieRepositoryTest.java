dcf76494605322cb49d5ab66f409187557626c00
Administrator <123>
2014-09-12 21:00:47 +0800

    Remove unused import

--- movietime-service/src/test/java/com/jimmystudio/movietime_service/repository/MovieRepositoryTest.java
+++ movietime-service/src/test/java/com/jimmystudio/movietime_service/repository/MovieRepositoryTest.java
@@ -8,10 +8,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.jimmystudio.movietime_service.entity.Movie;
-import com.jimmystudio.movietime_service.repository.MovieRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext.xml")
